9584c433269af4d5311ea32219617991cd092116
Henry <hleejr0103@gmail.com>
2020-07-10 13:56:24 -0700

    can output scraped data to json file

--- scrape.go
+++ scrape.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/gocolly/colly"
 )
+
+type playerInfo struct {
+	Name string `json:"name"`
+}
+
+func writeFile(data string) {
+	/*
+		Writes data onto file
+	*/
+	bytesToWrite := []byte(data)
+	err := ioutil.WriteFile("output.json", bytesToWrite, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -15,9 +32,14 @@ func main() {
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("#block-league-content > section.row.nba-player-index__row > section:nth-child(1) > a:nth-child(2) > p", func(e *colly.HTMLElement) {
+		testPlayer := playerInfo{Name: e.Text}
+		testJSON, _ := json.Marshal(testPlayer)
 
 		// Print player name
-		fmt.Printf("Player name: %q\n", e.Text)
+		// fmt.Printf("Player name: %q\n", e.Text)
+		// fmt.Printf("%s \n", testPlayer.Name)
+		// fmt.Printf("%s \n", string(testJSON))
+		writeFile(string(testJSON))
 	})
 
 	// Before making a request print "Visiting ..."
@@ -31,14 +53,6 @@ func main() {
 
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Visited", r.Request.URL)
-	})
-
-	c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
-		fmt.Println("First column of a table row:", e.Text)
-	})
-
-	c.OnXML("//h1", func(e *colly.XMLElement) {
-		fmt.Println(e.Text)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
