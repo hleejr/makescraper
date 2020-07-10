d26829a0a850a47f4678eedf035d3a7c2fd21a18
Henry <hleejr0103@gmail.com>
2020-07-10 12:54:23 -0700

    first commit

--- scrape.go
+++ scrape.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gocolly/colly"
 )
@@ -13,11 +14,10 @@ func main() {
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	c.OnHTML("#block-league-content > section.row.nba-player-index__row > section:nth-child(1) > a:nth-child(2) > p", func(e *colly.HTMLElement) {
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Print player name
+		fmt.Printf("Player name: %q\n", e.Text)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -25,6 +25,26 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited", r.Request.URL)
+	})
+
+	c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
+		fmt.Println("First column of a table row:", e.Text)
+	})
+
+	c.OnXML("//h1", func(e *colly.XMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://www.nba.com/players")
 }
